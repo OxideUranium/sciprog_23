36bb762ba7c0111b3b213636113b2a18dd260fff
Ou Kerman <47890698+OxideUranium@users.noreply.github.com>
2023-11-26 19:10:15 +0000

    practice 6 fix again
    The last commit only fixed half of the problems and ignored the other half. Now everything should be fixed

--- practice6/main.c
+++ practice6/main.c
@@ -9,25 +9,25 @@ void matrixMulti(double A[N][P], double B[P][Q], double C[N][Q]);
 
 int main() {
     double A[N][P], B[P][Q], C[N][Q];
+    int i,j;
 
     // Initialize A
-    int i;
     for (i = 0; i < N; ++i) {
-        for (int j = 0; j < P; ++j) {
+        for (j = 0; j < P; ++j) {
             A[i][j] = i + j; 
         }
     }
 
     // Initialize B
     for (i = 0; i< P; ++i) {
-        for (int j = 0; j<Q; ++j) {
+        for (j = 0; j<Q; ++j) {
             B[i][j] = i - j; // Bij = i - j
         }
     }
 
     // Initialize C 
     for (i=0; i<N; ++i) {
-        for (int j = 0; j<Q; ++j) {
+        for (j = 0; j<Q; ++j) {
             C[i][j] = 0.0;
         }
     }
@@ -40,7 +40,7 @@ int main() {
     // Print out the result
     printf("Matrix A:\n");
     for (i = 0; i<N; ++i) {
-        for (int j = 0; j<P; ++j) {
+        for (j = 0; j<P; ++j) {
             printf("%.2f ", A[i][j]);
         }
         printf("\n");
@@ -48,7 +48,7 @@ int main() {
 
     printf("\nMatrix B:\n");
     for (i = 0; i<P; ++i) {
-        for (int j = 0; j<Q; ++j) {
+        for (j = 0; j<Q; ++j) {
             printf("%.2f ", B[i][j]);
         }
         printf("\n");
@@ -56,7 +56,7 @@ int main() {
 
     printf("\nMatrix C (Result):\n");
     for (i = 0; i< N; ++i) {
-        for (int j = 0; j<Q; ++j) {
+        for (j = 0; j<Q; ++j) {
             printf("%.2f ", C[i][j]);
         }
         printf("\n");
