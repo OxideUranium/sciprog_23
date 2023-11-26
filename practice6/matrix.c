4cfa94b7de48a2644a37862fcbf83534560960eb
Ou Kerman <47890698+OxideUranium@users.noreply.github.com>
2023-11-26 19:05:15 +0000

    practice6 fix
    There is a problem with the definition of int i in the for loop, and the make command in the readme document has not been updated.

--- practice6/matrix.c
+++ practice6/matrix.c
@@ -4,10 +4,10 @@
 
 // Matrix multiplication
 void matrixMulti(double A[N][P], double B[P][Q], double C[N][Q]) {
-    int i;
+    int i,j,k;
     for (i=0; i< N; ++i) {
-        for (int j =0; j<Q; ++j) {
-            for (int k = 0; k<P; ++k) {
+        for (j =0; j<Q; ++j) {
+            for (k = 0; k<P; ++k) {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
